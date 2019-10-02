41becd0c00ea16dd237c44c3f7836788c463e641
Ahmad Guenoun <amguenoun@gmail.com>
2019-10-02 15:21:29 -0400

    created bird class

---
+++ src/animalkingdom/Bird.java
@@ -0,0 +1,23 @@
+package animalkingdom;
+
+public class Bird extends Animal {
+
+    public Bird(String name, int year) {
+        super(name, year);
+    }
+
+    @Override
+    public String move() {
+        return name + " moves by flying";
+    }
+
+    @Override
+    public String breathe() {
+        return name + " breathes through lungs";
+    }
+
+    @Override
+    public String reproduce() {
+        return name + " reproduces through eggs";
+    }
+}
